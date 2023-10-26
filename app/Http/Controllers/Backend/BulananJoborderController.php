f626e41cc30608b881f1f0804c6a241b1df344d1
henmusta <hendrimusta88@gmail.com>
2023-10-26 22:43:39 +0700

    sadsad

--- app/Http/Controllers/Backend/BulananJoborderController.php
+++ app/Http/Controllers/Backend/BulananJoborderController.php
@@ -79,10 +79,145 @@ class BulananJoborderController extends Controller
         $type = 'pdf';
         $getdata = $this->data($request, $type);
 
+        $html = '';
+
+
+        $no = 1;
+        for($i = 0; $i < count($getdata); $i++){
+           $html .= '<table id="pakettable">';
+              $html .= '<thead style="background-color: #fff !important; color:black;">
+                      <tr>
+                          <th width="10%" colspan="16" style="text-align: left !important">'.$getdata[$i]['bulan'].'</th>
+                      </tr>
+                  </thead>
+                  <thead style="background-color: #fff !important; color:black;">
+                      <tr>
+                          <th>No</th>
+                          <th class="text-center">Id JO</th>
+                          <th>Tanggal</th>
+                          <th>Status</th>
+                          <th>Driver</th>
+                          <th>No Plat Polisi</th>
+                          <th>Jenis Mobil</th>
+                          <th>Customer</th>
+                          <th>Muatan</th>
+                          <th>Alamat Awal (Dari)</th>
+                          <th>Alamat Akhir (Ke)</th>
+                          <th width="8%">Total Uj</th>
+                          <th>Pembayaran</th>
+                          <th width="8%">Sisa Uj</th>
+                          <th>Keterangan</th>
+                          <th>Operator (Waktu)</th>
+                      </tr>
+                  </thead>';
+            $html .= '<tbody>';
+
+            $alldata = $getdata[$i]['alldata']->get();
+            $total_uj=$sisa_uj = 0;
+            foreach($alldata as $val){
+                $status_payment = $alldata[$i]['status_payment'] == '0' ? 'Belum Bayar' : ($alldata[$i]['status_payment'] == '1' ? 'Progress Payment' : 'Lunas');
+                $status_jo = $alldata[$i]['status_joborder'] == '0' ? 'Ongoing' : 'Done';
+                $html .= '
+                <tr>
+                    <td class="text-center">{{$no++}}</td>
+                    <td><a href="'.route('backend.joborder.index').'?joborder_id='.$val->id.'" target="_blank">'.$val->kode_joborder.'</a></td>
+                    <td>'.$val->tgl_joborder.'</td>
+                    <td>'.$status_jo.'</td>
+                    <td>'.$val->driver['name'].'</td>
+                    <td>'.$val->mobil['nomor_plat'].'</td>
+                    <td>'.$val->jenismobil['name'].'</td>
+                    <td>'.$val->customer['name'].'</td>
+                    <td>'.$val->muatan['name'].'</td>
+                    <td>'.$val->ruteawal['name'].'</td>
+                    <td>'.$val->ruteakhir['name'].'</td>
+                    <td class="text-end">Rp. '. number_format($val->total_uang_jalan,0,',','.').'</td>
+                    <td>'.$status_payment.'</td>
+                    <td class="text-end">Rp. '. number_format($val->sisa_uang_jalan,0,',','.').'</td>
+                    <td width="5%">-</td>
+                    <td>-</td>
+                </tr>';
+            }
+             $html .= '</tbody>';
+
+           $html .= '</table>';
+        }
+
+        // @php($no=1)
+        // @php($getdata = $data['data'])
+        // @for ($item = 0; $item < count($getdata); $item++)
+        // {{-- {{dd($getdata[$item]['bulan'])}} --}}
+        // <table id="pakettable">
+        //     <thead style="background-color: #fff !important; color:black;">
+        //         <tr>
+        //             <th width="10%" colspan="16" style="text-align: left !important">{{$getdata[$item]['bulan']}}</th>
+        //         </tr>
+        //     </thead>
+        //     <thead style="background-color: #fff !important; color:black;">
+        //         <tr>
+        //             <th>No</th>
+        //             <th class="text-center">Id JO</th>
+        //             <th>Tanggal</th>
+        //             <th>Status</th>
+        //             <th>Driver</th>
+        //             <th>No Plat Polisi</th>
+        //             <th>Jenis Mobil</th>
+        //             <th>Customer</th>
+        //             <th>Muatan</th>
+        //             <th>Alamat Awal (Dari)</th>
+        //             <th>Alamat Akhir (Ke)</th>
+        //             <th width="8%">Total Uj</th>
+        //             <th>Pembayaran</th>
+        //             <th width="8%">Sisa Uj</th>
+        //             <th>Keterangan</th>
+        //             <th>Operator (Waktu)</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         @php($total_uj=$sisa_uj = 0)
+        //         @php($alldata = $getdata[$item]['alldata']->get())
+        //         @for ($i = 0; $i < count($alldata); $i++)
+        //         @php($status_payment = $alldata[$i]['status_payment'] == '0' ? 'Belum Bayar' : ($alldata[$i]['status_payment'] == '1' ? 'Progress Payment' : 'Lunas'))
+        //         @php($status_jo = $alldata[$i]['status_joborder'] == '0' ? 'Ongoing' : 'Done')
+        //         @php($total_uj += $alldata[$i]->total_uang_jalan)
+        //         @php($sisa_uj += $alldata[$i]->sisa_uang_jalan)
+        //             <tr>
+        //                 <td width="2%" class="text-center">{{$no++}}</td>
+        //                 <td>{{ $alldata[$i]['kode_joborder']}}</td>
+        //                 <td>{{ $alldata[$i]['tgl_joborder']}}</td>
+        //                 <td width="20px">{{$status_jo}}</td>
+        //                 <td>{{ $alldata[$i]['driver']->name }}</td>
+        //                 <td width="50px">{{ $alldata[$i]['mobil']['nomor_plat']}}</td>
+        //                 <td>{{ $alldata[$i]['jenismobil']->name }}</td>
+        //                 <td>{{ $alldata[$i]['customer']->name }}</td>
+        //                 <td>{{ $alldata[$i]['muatan']->name }}</td>
+        //                 <td>{{ $alldata[$i]['ruteawal']->name }}</td>
+        //                 <td>{{ $alldata[$i]['ruteakhir']->name }}</td>
+        //                 <td  width="60px" class="text-end">Rp. {{ number_format( $alldata[$i]['total_uang_jalan'],0,',','.')}}</td>
+        //                 <td>{{$status_payment}}</td>
+        //                 <td  class="text-end">Rp. {{ number_format( $alldata[$i]['sisa_uang_jalan'],0,',','.')}}</td>
+        //                 <td>{{ $alldata[$i]['keterangan_joborder']}}</td>
+        //                 <td>{{ $alldata[$i]['createdby']->name ?? '' }} ( {{  \Carbon\Carbon::parse( $alldata[$i]['created_at'])->format('d-m-Y H:i:s')  }} )</td>
+        //             </tr>
+        //         @endfor
+        //     </tbody>
+        //     <tfoot>
+        //         <tr>
+        //             <th colspan="11"style="text-align:right">Total: </th>
+        //             <th class="text-end" id="">Rp. {{ number_format($total_uj,0,',','.')}}</th>
+        //             <th></th>
+        //             <th class="text-end" id="">Rp. {{ number_format($sisa_uj,0,',','.')}}</th>
+        //             <th width="5%"></th>
+        //             <th width="5%"></th>
+        //          </tr>
+        //     </tfoot>
+        // </table><br>
+        // @endfor
+
         $data = [
             'bulan' => $request['bulan'],
             'tahun' => $request['tahun'],
             'data' =>$getdata ,
+            'datatable' =>$html ,
         ];
 
 
--- resources/views/backend/bulananjoborder/pdf.blade.php
+++ resources/views/backend/bulananjoborder/pdf.blade.php
@@ -78,76 +78,7 @@
         <p style=" text-align: center; margin-bottom: 0; font-size:8px;"> Print By : {{ Auth::user()->name ?? '' }} ( {{  \Carbon\Carbon::now()->format('d-m-Y H:i:s')  }} )</p>
     </div>
 
+    {!! $data['datatable']!!}
 
-    @php($no=1)
-    @php($getdata = $data['data'])
-    @for ($item = 0; $item < count($getdata); $item++)
-    {{-- {{dd($getdata[$item]['bulan'])}} --}}
-    <table id="pakettable">
-        <thead style="background-color: #fff !important; color:black;">
-            <tr>
-                <th width="10%" colspan="16" style="text-align: left !important">{{$getdata[$item]['bulan']}}</th>
-            </tr>
-        </thead>
-        <thead style="background-color: #fff !important; color:black;">
-            <tr>
-                <th>No</th>
-                <th class="text-center">Id JO</th>
-                <th>Tanggal</th>
-                <th>Status</th>
-                <th>Driver</th>
-                <th>No Plat Polisi</th>
-                <th>Jenis Mobil</th>
-                <th>Customer</th>
-                <th>Muatan</th>
-                <th>Alamat Awal (Dari)</th>
-                <th>Alamat Akhir (Ke)</th>
-                <th width="8%">Total Uj</th>
-                <th>Pembayaran</th>
-                <th width="8%">Sisa Uj</th>
-                <th>Keterangan</th>
-                <th>Operator (Waktu)</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php($total_uj=$sisa_uj = 0)
-            @php($alldata = $getdata[$item]['alldata']->get())
-            @for ($i = 0; $i < count($alldata); $i++)
-            @php($status_payment = $alldata[$i]['status_payment'] == '0' ? 'Belum Bayar' : ($alldata[$i]['status_payment'] == '1' ? 'Progress Payment' : 'Lunas'))
-            @php($status_jo = $alldata[$i]['status_joborder'] == '0' ? 'Ongoing' : 'Done')
-            @php($total_uj += $alldata[$i]->total_uang_jalan)
-            @php($sisa_uj += $alldata[$i]->sisa_uang_jalan)
-                <tr>
-                    <td width="2%" class="text-center">{{$no++}}</td>
-                    <td>{{ $alldata[$i]['kode_joborder']}}</td>
-                    <td>{{ $alldata[$i]['tgl_joborder']}}</td>
-                    <td width="20px">{{$status_jo}}</td>
-                    <td>{{ $alldata[$i]['driver']->name }}</td>
-                    <td width="50px">{{ $alldata[$i]['mobil']['nomor_plat']}}</td>
-                    <td>{{ $alldata[$i]['jenismobil']->name }}</td>
-                    <td>{{ $alldata[$i]['customer']->name }}</td>
-                    <td>{{ $alldata[$i]['muatan']->name }}</td>
-                    <td>{{ $alldata[$i]['ruteawal']->name }}</td>
-                    <td>{{ $alldata[$i]['ruteakhir']->name }}</td>
-                    <td  width="60px" class="text-end">Rp. {{ number_format( $alldata[$i]['total_uang_jalan'],0,',','.')}}</td>
-                    <td>{{$status_payment}}</td>
-                    <td  class="text-end">Rp. {{ number_format( $alldata[$i]['sisa_uang_jalan'],0,',','.')}}</td>
-                    <td>{{ $alldata[$i]['keterangan_joborder']}}</td>
-                    <td>{{ $alldata[$i]['createdby']->name ?? '' }} ( {{  \Carbon\Carbon::parse( $alldata[$i]['created_at'])->format('d-m-Y H:i:s')  }} )</td>
-                </tr>
-            @endfor
-        </tbody>
-        <tfoot>
-            <tr>
-                <th colspan="11"style="text-align:right">Total: </th>
-                <th class="text-end" id="">Rp. {{ number_format($total_uj,0,',','.')}}</th>
-                <th></th>
-                <th class="text-end" id="">Rp. {{ number_format($sisa_uj,0,',','.')}}</th>
-                <th width="5%"></th>
-                <th width="5%"></th>
-             </tr>
-        </tfoot>
-    </table><br>
-    @endfor
 </body>
 </html>
