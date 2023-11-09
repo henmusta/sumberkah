76ef5fbc634b09dcfd897b7a15921518f1419fd8
henmusta <hendrimusta88@gmail.com>
2023-11-09 23:11:09 +0700

    sdasadSDDD

--- app/Http/Controllers/Backend/ActivityLogController.php
+++ app/Http/Controllers/Backend/ActivityLogController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Activitylog;
+use App\Models\ActivityLog;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use App\Traits\ResponseStatus;
